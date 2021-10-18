0a6c97e83125c529dfce4500bd1fdac2f7925a7d
Abdullah Imam <aemam816@gmail.com>
2021-10-18 20:47:49 +0200

    Second Commit add controllers, views, models, static files

---
+++ BookList/Models/BookModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList.Models
+{
+	public class BookModel
+	{
+		public int ID { set; get; }
+		public string Name { set; get; }
+		public string Author { set; get; }
+		public string ISBN { set; get; }
+	}
+}
